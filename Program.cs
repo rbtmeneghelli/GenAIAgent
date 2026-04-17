8e3e1a905f1f602487892d5616e43b9b26ec2ee5
Roberto Meneghelli <roberto.mng.89@gmail.com>
2026-04-16 22:42:16 -0300

    Versão 1.0 - Criado console application com a biblioteca MAF para trabalhar com AI
    Commit (Versão 1.0 - Criado console application com a biblioteca MAF para trabalhar com AI)

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using GenAIAgent;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using OpenAI.Chat;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var openAPIKey = config["OpenAi:ApiKey"];
+
+var agent = new OpenAIClient(openAPIKey)
+            .GetChatClient("gpt-4o-mini")
+            .AsAIAgent("""Você é um assistente que pode fornecer informações sobre o clima.""", tools:
+            [
+                AIFunctionFactory.Create(WeatherTool.GetWeather)
+            ]);
+
+var agent2 = new OpenAIClient(openAPIKey)
+            .GetChatClient("gpt-4o-mini")
+            .AsAIAgent("""Você é um assistente que irá receber um texto em idioma português e ira traduzir o texto para o idioma em inglês""");
+
+var session = await agent.CreateSessionAsync();
+var session2 = await agent2.CreateSessionAsync();
+
+while (true)
+{
+    /* Respondendo igual ao chatGPT */
+    //await foreach (var token in agent.RunStreamingAsync(prompt ?? ))
+    //{
+    //    Console.WriteLine(token);
+    //}
+
+    Console.WriteLine("Faça uma pergunta:");
+    var prompt = Console.ReadLine();
+    var result = agent.RunAsync(prompt ?? string.Empty, session);
+
+    Console.WriteLine("Resposta em português:");
+    Console.WriteLine(result.Result.Text);
+
+    var translatedResult = agent2.RunAsync(result.Result.Text, session2);
+    Console.WriteLine("Resposta em inglês:");
+    Console.WriteLine(translatedResult.Result.Text);
+
+    Console.WriteLine("-------------------------------------------");
+}
---
+++ WeatherTool.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace GenAIAgent;
+
+public static class WeatherTool
+{
+    [Description("Gets the current weather for a specified location.")]
+    public static string GetWeather([Description("The location to get the weather for.")] string location)
+    {
+        return $"The current weather in {location} is sunny with a temperature of 25°C.";
+    }
+}
